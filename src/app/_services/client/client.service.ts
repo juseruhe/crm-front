3d0e02cd9e563c041d017e93fb90aab90b6121e0
Jaxbleydi <vipproyectosmacro@gmail.com>
2024-10-10 11:02:42 -0500

    cambios en el llamado de los servicios

--- src/app/_services/client/client.service.ts
+++ src/app/_services/client/client.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Client } from 'src/app/interface/client.model';
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ClientService {
 
 
@@ -19,23 +20,48 @@ export class ClientService {
 
   // Método para obtener todos los clientes
   getClients(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    const token = localStorage.getItem('token')
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json', // Indica que estás enviando datos JSON
+      'unoweb_authorization': `${token}`
+    });
+    return this.http.get<any>(this.apiUrl,{headers: headers});
   }
 
   getClientById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`); 
+    const token = localStorage.getItem('token')
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json', // Indica que estás enviando datos JSON
+      'unoweb_authorization': `${token}`
+    });
+    return this.http.get<any>(`${this.apiUrl}/${id}`,{headers: headers}); 
   }
 
   deleteClient(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+    const token = localStorage.getItem('token')
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json', // Indica que estás enviando datos JSON
+      'unoweb_authorization': `${token}`
+    });
+    return this.http.delete<any>(`${this.apiUrl}/${id}`,{headers: headers});
   }
 
   updateClient(id: number, clientData: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, clientData);
+    const token = localStorage.getItem('token')
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json', // Indica que estás enviando datos JSON
+      'unoweb_authorization': `${token}`
+    });
+    return this.http.put(`${this.apiUrl}/${id}`, clientData, {headers: headers});
   }
 
   createClient(clientData: Client): Observable<any> {
-    return this.http.post<any>(this.apiUrl, clientData);
+    const token = localStorage.getItem('token')
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json', // Indica que estás enviando datos JSON
+      'unoweb_authorization': `${token}`
+    });
+    return this.http.post<any>(this.apiUrl, clientData,{headers: headers});
   }
 
 
